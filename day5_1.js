18cbc7b9e2a0fd6d2bb94f4c0af6cd8934f2d8ec
citrusfrukt <rothgaard@gmail.com>
2020-12-06 16:12:56 +0100

    Day 5 - JS

---
+++ day5_1.js
@@ -0,0 +1,42 @@
+fs = require("fs");
+
+function getInput(){
+    let input = fs.readFileSync("day5_input.txt", "utf8").trim();
+    return input.split(/\r?\n/)
+}
+
+function getRow(boardingPass){
+    const rowCharacters = boardingPass.substring(0, 7);
+    const rowBinary = rowCharacters.replace(/F/g, 0).replace(/B/g, 1);
+    const rowDecimal = parseInt(rowBinary, 2);
+
+    if(isNaN(rowDecimal)){
+        throw new Error(`Invalid input: ${boardingPass}`)
+    }
+
+    return rowDecimal;
+}
+
+function getColumn(boardingPass){
+    const columnCharacters = boardingPass.substring(7, 10);
+    const columnBinary = columnCharacters.replace(/L/g, 0).replace(/R/g, 1);
+    const columnDecimal = parseInt(columnBinary, 2);
+
+    if(isNaN(columnDecimal)){
+        throw new Error(`Invalid input: ${boardingPass}`)
+    }
+
+    return columnDecimal;
+}
+
+function getSeatId(boardingPass){
+    const row = getRow(boardingPass);
+    const column = getColumn(boardingPass);
+
+    return (row * 8) + column; 
+}
+
+const boardingPasses = getInput();
+const seatIds = boardingPasses.map(getSeatId)
+const maxId = Math.max(...seatIds);
+console.log(maxId)
